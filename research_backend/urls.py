61588a9400adea2c1b50ab96cac4ffe4d61a7f38
Mehmet Yusuf Ocak <yusuf.ock2611@gmail.com>
2025-12-06 16:24:43 +0300

    Fix: Redirect root URL to Swagger docs

--- research_backend/urls.py
+++ research_backend/urls.py
@@ -1,21 +1,7 @@
-"""
-URL configuration for research_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView # <--- YENİ IMPORT
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -23,17 +9,13 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
+    # 🔹 ANASAYFA YÖNLENDİRMESİ (YENİ)
+    # Siteye giren direk dokümantasyona gitsin
+    path('', RedirectView.as_view(url='/api/docs/', permanent=False)),
+
     path('admin/', admin.site.urls),
-
-    # 🔹 OpenAPI şema (JSON)
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-
-    # 🔹 Swagger UI
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-
-    # 🔹 Redoc (istersen kullan)
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
-    # 🔹 Bizim asıl API endpoint'leri
     path('api/', include('core.urls')),
 ]
